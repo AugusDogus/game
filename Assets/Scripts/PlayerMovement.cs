bf5d19aef71eaac2465b34e39cad0b419e1bf622
Augie <augie@luebbers.email>
2019-08-26 14:27:33 -0500

    Added attacking and moving to player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,18 +20,53 @@ public class PlayerMovement : MonoBehaviour
     // Setup our player
     void Start()
     {
-        // Set our default state to walking/idling
         currentState = PlayerState.walk;
-        // Get our components
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
-        // Set our default attack direction to down
-        animator.SetFloat("moveX", 0);
-        animator.SetFloat("moveY", -1);
     }
 
     void Update()
     {
-        
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        change = change.normalized;
+        if (Input.GetButtonDown("Attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        } else if (currentState == PlayerState.walk)
+        {
+            UpdateAimation();
+        }
+    }
+
+    void UpdateAimation()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(0.33f);
+        currentState = PlayerState.walk;
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + change * speed * Time.fixedDeltaTime);
     }
 }
