a2e9fb13aa85bf3b255f740c09c161df8064b14e
Augie <augie@luebbers.email>
2019-08-26 19:34:49 -0500

    Added breakable pots

---
+++ Assets/Scripts/BreakableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Break()
+    {
+        anim.SetBool("breaking", true);
+        StartCoroutine(BreakCo());
+    }
+
+    IEnumerator BreakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+
+}
